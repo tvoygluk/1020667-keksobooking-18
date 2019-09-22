fe96e7e0853bc46c3bd1a4c1a81554467d5223dc
Maxim Medyanik <tvoygluk@gmail.com>
2019-09-23 00:27:11 +0300

    Добавил для генерации моков функцию создания массива случайной длины из переставленных элементов

--- js/main.js
+++ js/main.js
@@ -30,7 +30,42 @@
 
 var makeMocks = function () {
   var MOCKS_LENGTH = 8;
+  var CHECKIN_ARRAY = ['12:00', '13:00', '14:00'];
+  var CHECKOUT_ARRAY = ['12:00', '13:00', '14:00'];
+  var APARTMENT_TYPE_ARRAY = ['palace', 'flat', 'house', 'bungalo'];
+  var FEATURES_ARRAY = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var PHOTO_ADDRESS_ARRAY = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
   var mocksArray = [];
+
+  var getRandomArrayElement = function (someArray) {
+    return someArray[Math.floor(Math.random() * someArray.length)];
+  };
+
+  var shakeArray = function (array) {
+    var newLength = array.length;
+    var buffer;
+    var currentRandomLength;
+
+    while (newLength) {
+      currentRandomLength = Math.floor(Math.random() * newLength--);
+      buffer = array[newLength];
+      array[newLength] = array[currentRandomLength];
+      array[currentRandomLength] = buffer;
+    }
+
+    return array;
+  };
+
+  var makeArrayRandomShorter = function (str) {
+    var array = str.split(', ');
+    array = str.split(', ', Math.floor(Math.random() * array.length));
+
+    return array;
+  };
+
+  var makeNewShorterShakeArray = function (array) {
+    return makeArrayRandomShorter(shakeArray(array).join(', '));
+  };
 
   for (var i = 0; i < MOCKS_LENGTH; i++) {
     var mockObj = {
@@ -42,14 +77,14 @@ var makeMocks = function () {
         'title': '',
         'address': '{{location.x}}, {{location.y}}',
         'price': 0,
-        'type': 'palace, flat, house или bungalo',
+        'type': getRandomArrayElement(APARTMENT_TYPE_ARRAY),
         'rooms': 0,
         'guests': 0,
-        'checkin': 'строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00',
-        'checkout': 'строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00',
-        'features': [], /* массив строк случайной длины из ниже предложенных: 'wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner' */
+        'checkin': getRandomArrayElement(CHECKIN_ARRAY),
+        'checkout': getRandomArrayElement(CHECKOUT_ARRAY),
+        'features': makeNewShorterShakeArray(FEATURES_ARRAY),
         'description': '',
-        'photos': [] /* массив строк случайной длины, содержащий адреса фотографий 'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg' */
+        'photos': makeNewShorterShakeArray(PHOTO_ADDRESS_ARRAY)
 
       },
 
@@ -57,8 +92,8 @@ var makeMocks = function () {
         'x': 'случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.',
         'y': 'случайное число, координата y метки на карте от 130 до 630'
       }
-
     };
+
     mocksArray.push(mockObj);
   }
 
