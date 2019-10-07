6348cc1de31cb2c6759e734e267224c368d782b5
Maxim Medyanik <tvoygluk@gmail.com>
2019-10-07 20:06:47 +0300

    Поправил функцию вывода числительных

--- js/main.js
+++ js/main.js
@@ -49,12 +49,6 @@ var getRightCase = function (inputNumber, firstCase, secondCase, thirdCase) {
       case 2:
       case 3:
       case 4: return secondCase;
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 0: return thirdCase;
     }
   }
   return thirdCase;
